2e0b30bc62bee16254996fb6b105c2b3a724947c
bipinjoshi <bipinjoshicr712@gmail.com>
2023-11-12 16:39:37 +0530

    started working insinde chat body

--- src/component/Chat.js
+++ src/component/Chat.js
@@ -1,8 +1,39 @@
 import React from "react";
 import "./css/Chat.css";
+import { Avatar, IconButton } from "@mui/material";
+import { useState, useEffect } from "react";
+import { AttachFile, MoveDown, SearchOffOutlined } from "@mui/icons-material";
 
 const Chats = () => {
-  return <div className="chats">hello</div>;
+  const [name, setname] = useState("");
+  useEffect(() => {
+    setname(Math.floor(Math.random() * 5000));
+  }, [""]);
+  return (
+    <div className="chats">
+      <div className="header">
+        <Avatar
+          src={`https://api.dicebear.com/7.x/pixel-art/svg?seed=${name}`}
+        />
+        <div className="header_info">
+          <h2>class</h2>
+          <p>last seen</p>
+        </div>
+        <div className="head_right">
+          <IconButton>
+            <SearchOffOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoveDown />
+          </IconButton>
+        </div>
+      </div>
+      <div className="chat_body"></div>
+    </div>
+  );
 };
 
 export default Chats;
--- src/component/Sidebar.js
+++ src/component/Sidebar.js
@@ -1,10 +1,9 @@
 import React from "react";
 import "./css/Sidebar.css";
 import ChatIcon from "@mui/icons-material/Chat";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import DonutLargeIcon from "@mui/icons-material/DonutLarge";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
-import { IconButton } from "@mui/material";
+import { Avatar, IconButton } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import SidebarChat from "./SidebarChat";
 
@@ -13,7 +12,7 @@ const Sidebar = () => {
     <div className="content">
       <div className="header">
         <IconButton>
-          <AccountCircleIcon />
+          <Avatar />
         </IconButton>
 
         <div className="heder_left">
